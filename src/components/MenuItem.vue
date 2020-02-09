b553e1db9afb82a48c644b33d6edc844e74d3fcd
Roman Asadov <roma-asadov@mail.ru>
2020-02-09 13:07:13 +0300

    Исправил ошибку с пропсами

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -23,7 +23,7 @@
         async created() {
             const axios = new AxiosWrapper();
             let icon = null;
-            await axios.request('http://geo.tatar.ru/api/layers/' + this.props.guid, 'get', {headers: {'X-App': '51d5e291a5f80482', 'Content-Type': 'application/json'}}).then(function (response) {
+            await axios.request('http://geo.tatar.ru/api/layers/' + this.$props.guid, 'get', {headers: {'X-App': '51d5e291a5f80482', 'Content-Type': 'application/json'}}).then(function (response) {
                 icon = response.data.styleHash.default[0].iconUrl;
             });
             this.icon = icon;
