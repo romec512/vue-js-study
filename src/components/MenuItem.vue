c15cf27fb39cd7976f506a5c10753e0226004736
Roman Asadov <roma-asadov@mail.ru>
2020-02-12 15:15:43 +0300

    Переделал под квазарную верстку

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,6 +1,16 @@
 <template>
-    <div id="menu-item">
-        <q-icon size="md"  :name="'img:' + icon"></q-icon>
+
+    <div>
+    <q-item>
+        <q-item-section avatar>
+            <q-icon size="md" :name="'img:' + icon" />
+        </q-item-section>
+        <q-item-section class="layer-name">{{layerName}}</q-item-section>
+        <q-item-section side>
+            <q-toggle v-model="selected" @input="changeValue"></q-toggle>
+        </q-item-section>
+    </q-item>
+    <q-separator inset="" />
     </div>
 </template>
 
@@ -18,20 +28,30 @@
         data() {
           return {
               icon: '',
+              layerName: '',
+              selected: false
           };
         },
         async created() {
             const axios = new AxiosWrapper();
-            let icon = null;
-            await axios.request('http://geo.tatar.ru/api/layers/' + this.$props.guid, 'get', {headers: {'X-App': '51d5e291a5f80482', 'Content-Type': 'application/json'}}).then(function (response) {
-                icon = response.data.styleHash.default[0].iconUrl;
+            let response = null;
+            await axios.request('http://geo.tatar.ru/api/layers/' + this.$props.guid, 'get', {headers: {'X-App': '51d5e291a5f80482', 'Content-Type': 'application/json'}}).then(function (_response) {
+                response = _response;
             });
-            this.icon = icon;
-            window.console.log(this.icon);
+            this.icon = response.data.styleHash.default[0].iconUrl;
+            this.layerName = response.data.name;
+            window.console.log(response);
+        },
+        methods: {
+            changeValue() {
+                //ToDo: сделать отображение и скрытие объектов на карте
+            }
         }
     }
 </script>
 
 <style scoped>
-
+    .layer-name {
+        font-size: 1.2em;
+    }
 </style>
