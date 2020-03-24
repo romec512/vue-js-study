e4a752a34244931f113d6e6c275199fa775dd3fe
Roman Asadov <roma-asadov@mail.ru>
2020-03-24 11:57:33 +0300

    Добавил слой "пляжи"

--- src/config/layers.js
+++ src/config/layers.js
@@ -1,4 +1,5 @@
 export default [
     'b2065fe9-5c7e-46c3-8cc5-0ccc89db129a',
-    'd16db581-6cf3-4ca8-925e-8e41293412ae'
+    'd16db581-6cf3-4ca8-925e-8e41293412ae',
+    'ae9a1830-da43-458b-b708-d58acd55f012'
 ]
